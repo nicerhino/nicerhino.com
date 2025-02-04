218157b7063e15aee07a601b5b5bcfd734f94e33
Pete Hayes <pete@hayes.id.au>
2025-02-04 13:16:30 +1100

    Add Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import "@fontsource/poppins/400.css";
 import "@fontsource/poppins/700.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Nice Rhino - A champion by your side",
@@ -21,6 +22,7 @@ export default function RootLayout({
         className="bg-slate-950 text-slate-200 scroll-smooth antialiased"
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );

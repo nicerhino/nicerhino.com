994a3b6d70f0445e99a1ea90c2393348acdce6ca
Pete Hayes <pete@hayes.id.au>
2025-04-17 10:23:36 +1000

    Fix Advisory x scroll

--- app/advisory/page.tsx
+++ app/advisory/page.tsx
@@ -75,7 +75,7 @@ function Hero() {
 
 function Summary() {
   return (
-    <section id="summary" className="relative py-24 sm:py-32">
+    <section id="summary" className="relative my-24 sm:my-32 overflow-x-clip">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-5">
           <h2 className="col-span-2 text-4xl font-semibold tracking-tight text-pretty sm:text-5xl">
